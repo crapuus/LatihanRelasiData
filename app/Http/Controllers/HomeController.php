a64e9d51461d07dfb95784455eb8da0123cb8e54
crapuus <choirulrizqy2017@gmail.com>
2021-02-24 22:40:56 +0700

    Belajar relasi data sederhana

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+Use App\User;
+class HomeController extends Controller
+{
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
+    public function index() {
+        $users = User::get();
+        return view('home', ['users' => $users]);
+    }
+    
+}
